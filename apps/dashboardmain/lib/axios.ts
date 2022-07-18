bd4b7c73a76f626815d864cb5660a6fe39d98d69
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-18 18:25:15 +0100

    cors anywhere

--- apps/dashboardmain/lib/axios.ts
+++ apps/dashboardmain/lib/axios.ts
@@ -1,7 +1,8 @@
 import Axios from 'axios'
 
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
 const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: PROXY_URL + process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 })
 

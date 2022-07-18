d27884f76ef5118fca2a6b1f93034900bf059070
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-18 18:28:53 +0100

    Revert "cors anywhere"
    This reverts commit bd4b7c73a76f626815d864cb5660a6fe39d98d69.

--- apps/dashboardmain/lib/axios.ts
+++ apps/dashboardmain/lib/axios.ts
@@ -1,8 +1,7 @@
 import Axios from 'axios'
 
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
 const axios = Axios.create({
-  baseURL: PROXY_URL + process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 })
 
